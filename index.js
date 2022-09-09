53e0dd6c94c54b3451e03542237687f180194d7a
mzeeshansikander <zeeshan.sikander@live.com>
2022-09-09 15:57:10 +0500

    craeted index.js file with all the required libraries

--- index.js
+++ index.js
@@ -3,10 +3,20 @@ const app=express();
 const port =process.env.PORT || 3000;
 const bodyParser = require("body-parser");
 const cors=require("cors");
+const mysql=require("./connection").con
 
 app.use(cors());
 app.use(express.json())
 app.use(bodyParser.urlencoded({extended:true}));
+
+app.get("/",(req,res)=>{
+    let qry="INSERT INTO contact_db (name,email,contact) VALUES ('mariam','mariam@gmail.com','0339889')"
+// res.send("hello world")
+mysql.query(qry,(err,results)=>{
+    console.log("error",err)
+    console.log("result",results)
+})
+})
 
 app.listen(port,()=>{
     console.log(`server is runnig on port ${port}`)
